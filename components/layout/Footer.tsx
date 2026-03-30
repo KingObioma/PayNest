d35a468795efbdca5d62880251f18e7714727b2d
king <kelvinozak0@gmail.com>
2026-03-30 22:40:10 +0100

    first commit

--- components/layout/Footer.tsx
+++ components/layout/Footer.tsx
@@ -73,7 +73,7 @@ export default function Footer() {
                 <Link href="/buy/scratch-cards" className="hover:text-primary-400 transition-colors">Scratch Cards</Link>
               </li>
               <li>
-                <Link href="/dashboard" className="hover:text-primary-400 transition-colors">Bill Payment</Link>
+                <Link href="/dashboard" className="hover:text-primary-400 transition-colors">Bill king Payment</Link>
               </li>
             </ul>
           </div>
