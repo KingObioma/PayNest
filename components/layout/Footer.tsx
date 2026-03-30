4aa543725df32649fa0f48bcacf2e05b937da0a7
king <kelvinozak0@gmail.com>
2026-03-30 22:41:46 +0100

    first commit

--- components/layout/Footer.tsx
+++ components/layout/Footer.tsx
@@ -64,7 +64,7 @@ export default function Footer() {
             <h3 className="text-white font-semibold mb-4">Services</h3>
             <ul className="space-y-2">
               <li>
-                <Link href="/dashboard" className="hover:text-primary-400 transition-colors">Airtime</Link>
+                <Link href="/dashboard" className="hover:text-primary-400 transition-colors">Airtime king</Link>
               </li>
               <li>
                 <Link href="/dashboard" className="hover:text-primary-400 transition-colors">Data Bundles</Link>
