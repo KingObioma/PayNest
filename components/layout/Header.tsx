c1acf9a5a7acffc86ee2a08de9b39ed0a6f2c53e
king <kelvinozak0@gmail.com>
2026-03-31 11:29:57 +0100

    first commit

--- components/layout/Header.tsx
+++ components/layout/Header.tsx
@@ -10,7 +10,7 @@ interface HeaderProps {
 }
 
 export default function Header({ showAuthButtons = true }: HeaderProps) {
-  const [isMenuOpen, setIsMenuOpen] = useState(true);
+  const [isMenuOpen, setIsMenuOpen] = useState(false);
   const [isDark, setIsDark] = useState(false);
 
   const toggleTheme = () => {
