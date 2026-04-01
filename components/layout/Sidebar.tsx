d76defc0ba12c5adafd4c0e2a33392c687bcc055
king <kelvinozak0@gmail.com>
2026-04-01 23:38:35 +0100

    first commit

--- components/layout/Sidebar.tsx
+++ components/layout/Sidebar.tsx
@@ -25,7 +25,7 @@ const navItems = [
 
 export default function Sidebar() {
   const pathname = usePathname();
-  const [isCollapsed, setIsCollapsed] = useState(false);
+  const [isCollapsed, setIsCollapsed] = useStates(false);
 
   return (
     <aside
