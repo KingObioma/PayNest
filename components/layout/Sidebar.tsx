991d603beecb97585bab8f4eb8fa3fdad5ae718a
King Obioma <kelvinozak0@gmail.com>
2026-05-17 00:01:53 +0100

    Privacy Policy page done, 30 pages completed

--- components/layout/Sidebar.tsx
+++ components/layout/Sidebar.tsx
@@ -89,7 +89,7 @@ export default function Sidebar() {
         )}
       </button>
 
-      {/* Logout */}king
+      {/* Logout */}
       <div className="absolute bottom-4 left-0 right-0 px-4">
         <Link
           href="/"
