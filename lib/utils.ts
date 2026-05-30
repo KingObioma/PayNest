7e2e06bc6d75e38ff6c9d4dcaa448cb49487b84a
King Obioma <kelvinozak0@gmail.com>
2026-05-30 23:51:28 +0100

    Fix build issues

--- lib/utils.ts
+++ lib/utils.ts
@@ -3,7 +3,7 @@ import { clsx, type ClassValue } from 'clsx';
 export function cn(...inputs: ClassValue[]) {
   return clsx(inputs);
 }
-
+obioma
 export function formatCurrency(amount: number, symbol: string = '₦'): string {
   return `${symbol}${amount.toLocaleString('en-NG', {
     minimumFractionDigits: 3,
